83fb2f0454d0329c35beb9dafeb7a8fc336e483f
crisypsodium <christinena.316@gmail.com>
2020-06-05 00:02:42 -0700

    Updates

---
+++ precache-manifest.e7d0d483269974dba6285e1255208c7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7da17653feb7e5d5a315e0c7cf3158e",
+    "url": "/crispy_directory/index.html"
+  },
+  {
+    "revision": "4436aef043b2a485868c",
+    "url": "/crispy_directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc75aff07701375930cd",
+    "url": "/crispy_directory/static/js/2.f85f06e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crispy_directory/static/js/2.f85f06e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4436aef043b2a485868c",
+    "url": "/crispy_directory/static/js/main.a3a29901.chunk.js"
+  },
+  {
+    "revision": "96e6e2d9aed1ea2c0ae8",
+    "url": "/crispy_directory/static/js/runtime-main.45f3b77f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crispy_directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
